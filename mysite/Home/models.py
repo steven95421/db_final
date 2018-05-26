c4d828e1f69c18788c79f6546622a8c2a9183c9e
leoleo30539 <30406472+leoleo30539@users.noreply.github.com>
2018-05-26 20:54:11 +0800

    Update models.py

--- mysite/Home/models.py
+++ mysite/Home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import CharField, Model
+from django_mysql.models import ListCharField
 
 # Create your models here.
 
@@ -17,4 +19,30 @@ class Announcement(models.Model):
     image = models.URLField(blank=True)
     markdown_text = models.TextField(blank=True)
 
-
+class Team(models.Model):
+    teamid = models.AutoField(primary_key=True)
+    teamname = models.CharField(max_length=100)
+    studentid = ListCharField(
+        base_field=CharField(max_length=100),
+        size=20,
+        max_length=(20 * 101)  # 20 * 100 character nominals, plus commas
+    )
+    studentname = ListCharField(
+        base_field=CharField(max_length=100),
+        size=20,
+        max_length=(20 * 101)  # 20 * 100 character nominals, plus commas
+    )
+    event = ListCharField(
+        base_field=CharField(max_length=100),
+        size=20,
+        max_length=(20 * 101)  # 20 * 100 character nominals, plus commas
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Post(models.Model):
+	    user_id = models.UUIDField(editable=False, unique=True)
+	    user_password = models.CharField(max_length=20)
+	    user_email = models.CharField(max_length=100)
+	    is_admin = models.BooleanField
+    
+    
