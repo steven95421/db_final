72ed8485a56521de47d220e5ac11a4d5a43dcc85
steven95421 <steven95421@gmail.com>
2018-05-26 18:55:48 +0800

    -v 0.11

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_final',
         'USER': 'root',
-        'PASSWORD': 'rig521icy0845106',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
