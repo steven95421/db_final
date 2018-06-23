a609d7e2b6308d5beca9969dd0e626b488860124
eternalding <39581323+eternalding@users.noreply.github.com>
2018-06-24 03:29:57 +0800

    Not yet finished Announcement
    Announcement not done yet. Unsolved problem described in comment

---
+++ Home/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from Home.models import Announcement
+from Home.models import event
+
+
+def home(request):
+    Announcement_list = Announcement.objects.all()
+    return render(request, 'home.html', {
+        'Announcement_list': Announcement_list,
+    })
+
+
+def signup(request, id):
+    event_signup = event.objects.get(id=id)
+    return render(request, 'signup.html', {'event_signup': event_signup})
+
+
+def events(request):
+    event_list = event.objects.all()
+    return render(request, 'events.html', {
+        'event_list': event_list,
+    })
+
+
+def login(request):
+    return render(request, './registration/login.html')
+
+
+def anncs(request):
+    return render(request, 'anncs.html')
+
+
+def admin(request):
+    return render(request, 'admin.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+def Announcement_detail(request, id):
+    anncs_id = Announcement.objects.get(id=id)
+    return render(request, 'anncs.html', {'anncs': anncs})
